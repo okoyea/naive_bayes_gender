8842e017591cbc91cdc884dab8b0e1f00d8e5374
O'Koyea Huff-Boone <okoyeahb@gmail.com>
2015-04-02 01:25:58 -0500

    refactor helper spec

--- spec/tasks/training_data_rake_helper_spec.rb
+++ spec/tasks/training_data_rake_helper_spec.rb
@@ -2,33 +2,52 @@ require 'rails_helper'
 require 'spec_helper'
 
 describe 'training_data helper for rake task' do
-  describe 'when the data is valid' do
+
+  context 'when the data is valid' do
     before do
       @full = Dir.pwd + "/spec/support/valid.json"
       root = File.expand_path("~/") + "/"
       @full.slice!(root)
+
+      path = File.expand_path("~/") + "/" + @full
+      @file = File.open("#{path}").read
+    end
+
+    let(:helper) { TrainingDataHelper.new(@full) }
+    let(:test_json) { JSON.parse(@file) }
+
+    it 'should properly import the file ' do
+      helper.get_json
+      expect(helper.json).to eq(test_json)
     end
 
     it 'should import all people' do
-      helper = TrainingDataHelper.new(@full)
       expect{helper.parse_file}.to change(Person, :count).by(2)
     end
 
     it 'should not raise an error' do
-        helper = TrainingDataHelper.new(@full)
       expect{helper.parse_file}.to_not raise_error
     end
   end
 
-  describe 'when the data is invalid' do
+  context 'when the data is invalid' do
     before do
       @full = Dir.pwd + "/spec/support/invalid.json"
       root = File.expand_path("~/") + "/"
       @full.slice!(root)
+      path = File.expand_path("~/") + "/" + @full
+      @file = File.open("#{path}").read
+    end
+
+    let(:helper) { TrainingDataHelper.new(@full) }
+    let(:test_json) { JSON.parse(@file) }
+
+    it 'should properly import the file ' do
+      helper.get_json
+      expect(helper.json).to eq(test_json)
     end
 
     it 'should raise an error' do
-      helper = TrainingDataHelper.new(@full)
       expect{helper.parse_file}.to raise_error(ArgumentError)
     end
   end
