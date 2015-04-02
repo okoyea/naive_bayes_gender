621a2cca4148dff6741dbb10335a0363d9dcf35e
O'Koyea Huff-Boone <okoyeahb@gmail.com>
2015-04-02 02:34:56 -0500

    cleanup spaces and rename some variables

--- spec/lib/training_data_rake_helper_spec.rb
+++ spec/lib/training_data_rake_helper_spec.rb
@@ -3,25 +3,26 @@ require 'spec_helper'
 
 describe 'Training data helper for training_data:import rake task' do
   context 'when the data is valid' do
-    let(:helper) { TrainingDataHelper.new(@full) }
-    let(:test_json) { JSON.parse(@file) }
+    let(:helper) { TrainingDataHelper.new(@full_path) }
+    let(:test_json) { JSON.parse(@test_file) }
 
     before do
-      @full = Dir.pwd + "/spec/support/valid.json"
-      root = File.expand_path("~/") + "/"
-      @full.slice!(root)
-      path = File.expand_path("~/") + "/" + @full
-      @file = File.open("#{path}").read
+      @full_path = Dir.pwd + "/spec/support/valid.json"
+      root_path = File.expand_path("~/") + "/"
+      @full_path.slice!(root_path)
+
+      test_path = File.expand_path("~/") + "/" + @full_path
+      @test_file = File.open("#{test_path}").read
     end
 
-    it 'should properly import the file ' do
+    it 'should properly import the file' do
       helper.get_json
       expect(helper.json).to eq(test_json)
     end
 
     it 'should import all people' do
       silence_stream(STDOUT) do
-        expect{helper.parse_file}.to change(Person, :count).by(2)
+        expect{ helper.parse_file }.to change(Person, :count).by(2)
       end
     end
 
@@ -33,24 +34,25 @@ describe 'Training data helper for training_data:import rake task' do
   end
 
   context 'when the data is invalid' do
-    let(:helper) { TrainingDataHelper.new(@full) }
-    let(:test_json) { JSON.parse(@file) }
+    let(:helper) { TrainingDataHelper.new(@full_path) }
+    let(:test_json) { JSON.parse(@test_file) }
 
     before do
-      @full = Dir.pwd + "/spec/support/invalid.json"
-      root = File.expand_path("~/") + "/"
-      @full.slice!(root)
-      path = File.expand_path("~/") + "/" + @full
-      @file = File.open("#{path}").read
+      @full_path = Dir.pwd + "/spec/support/invalid.json"
+      root_path = File.expand_path("~/") + "/"
+      @full_path.slice!(root_path)
+
+      test_path = File.expand_path("~/") + "/" + @full_path
+      @test_file = File.open("#{test_path}").read
     end
 
-    it 'should properly import the file ' do
+    it 'should properly import the file' do
       helper.get_json
       expect(helper.json).to eq(test_json)
     end
 
     it 'should raise an error' do
-      expect{helper.parse_file}.to raise_error(ArgumentError)
+      expect{ helper.parse_file }.to raise_error(ArgumentError)
     end
   end
 end
